1cf1f88f10e2f633663a16664f75bba33ec34d22
addianouar <anouareloddy@gmail.com>
2025-10-08 01:23:18 +0200

    compressed

---
+++ resize.js
@@ -0,0 +1,18 @@
+import fs from "fs";
+import path from "path";
+import sharp from "sharp";
+
+const inputFolder = path.join("src/assets/saralowe");
+const outputFolder = path.join("src/assets/saralowe/resized");
+
+if (!fs.existsSync(outputFolder)) fs.mkdirSync(outputFolder);
+
+fs.readdirSync(inputFolder).forEach(file => {
+  if (/\.(jpe?g|png)$/i.test(file)) {
+    sharp(path.join(inputFolder, file))
+      .resize(1200, 1500, { fit: "inside" }) // Resize proportionally
+      .toFile(path.join(outputFolder, file.replace(/\.(jpe?g|png)$/i, ".webp")))
+      .then(() => console.log(`Resized ${file}`))
+      .catch(err => console.error(err));
+  }
+});
